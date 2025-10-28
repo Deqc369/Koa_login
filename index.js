6e20abd580c84552d71a44ba9751af23d5c990dd
unknown <dhs9805@gmail.com>
2025-10-28 13:09:03 -0500

    se ha agregado koa-router

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 import Koa from 'koa'
+import Router from 'koa-router'
 import { setFinalResponseMdw, setResponseTimeMdw } from './middlewares.js'
 
 const app = new Koa()
+const router = new Router()
 
-// Piso 1 (primer middleware)
 app.use(setFinalResponseMdw)
-
-// Piso 2 (segundo middleware)
 app.use(setResponseTimeMdw)
 
-// Piso 3 (tercer middleware)
-app.use(async ctx => {
-  console.log('pasa5')
-  ctx.body = 'Hello World'
+app
+  .use(router.routes())
+  .use(router.allowedMethods())
+
+router.get('/user', (ctx, next) => {
+  ctx.body = { ok: true, message: 'Hello desde GET' }
+})
+
+router.post('/user', (ctx, next) => {
+  ctx.body = { ok: true, message: 'Hello desde POST' }
+})
+
+router.put('/user', (ctx, next) => {
+  ctx.body = { ok: true, message: 'Hello desde PUT' }
+})
+
+router.delete('/user', (ctx, next) => {
+  ctx.body = { ok: true, message: 'Hello desde DELETE' }
 })
 
 app.listen(3000, () => console.log('Server is running on http://localhost:3000'))
